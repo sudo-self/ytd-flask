f31fc16efa3efcdf63a39c9c057a7ea3c99bc05e
Sudo-Self <tech@jessejesse.com>
2024-11-03 05:37:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, send_file
 import yt_dlp
 import os
+import ffmpeg
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the YouTube Audio Downloader! Use the /download endpoint to download audio."
+    return "YT Ringtones"
 
 @app.route('/download', methods=['POST'])
 def download_audio():
@@ -16,21 +17,37 @@ def download_audio():
     if not url:
         return jsonify({'error': 'No URL provided'}), 400
 
+
     ydl_opts = {
-        'format': 'm4a/bestaudio/best',
+        'format': 'bestaudio/best',
+        'outtmpl': './downloads/%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'm4a',
         }],
-        'outtmpl': './downloads/%(title)s.%(ext)s'
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
-            file_path = ydl.prepare_filename(info_dict).replace('.webm', '.m4a')
+            m4a_file_path = ydl.prepare_filename(info_dict).replace('.webm', '.m4a')
 
-        return send_file(file_path, as_attachment=True)
+            # Convert the m4a file to .mp3
+            mp3_file_path = m4a_file_path.replace('.m4a', '.mp3')
+            ffmpeg.input(m4a_file_path).output(mp3_file_path).run(overwrite_output=True)
+
+            # Convert the m4a file to .m4r 
+            m4r_file_path = m4a_file_path.replace('.m4a', '.m4r')
+            ffmpeg.input(m4a_file_path, ss=0, t=20).output(m4r_file_path).run(overwrite_output=True)
+
+        # Returning both files as attachments
+        return jsonify({
+            'message': 'Files processed successfully.',
+            'files': {
+                'mp3': mp3_file_path,
+                'm4r': m4r_file_path
+            }
+        })
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -38,4 +55,5 @@ def download_audio():
 if __name__ == '__main__':
     os.makedirs('./downloads', exist_ok=True)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
 
